723766b0ba7f2e232ef903d96f4bab8aacadd1c6
chien2002222 <chiendvgch200793@fpt.edu.vn>
2022-09-24 16:17:12 +0700

    ok

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const fs = require("fs");
 const http = require("http")
 const port = process.env.PORT || 3000;
 const app = http.createServer((req,res)=>{
     res.statusCode = 200; //http_ok
     res.setHeader("Content-Type","Text/html")
-    res.write("<h1> Nguoi nghien Pham Trung Kien</h1>")
-    res.write('<img src = "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg" >')
-    res.end("Hello world")
+    if(req.url === "/"){
+        fs.readFile("./index.html", (err,data)=>{
+            if(err){
+                console.error(err)
+            }else{
+                res.write(data)
+                res.end()
+            }
+        })
+    }else if(req.url === "/greenwich"){
+        fs.readFile("./greenwich.html", (err,data)=>{
+            if(err){
+                console.error(err)
+            }else{
+                res.write(data)
+                res.end()
+            }
+        })
+    }else{
+        res.end("<h1>Page not found</h1>")
+    }
+    
 })
 app.listen(port)
